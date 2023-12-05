b1baea777ec9341e6270a14f43c8729ee9839a2d
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-12-06 04:33:29 +0700

    fix: change bg color, added padding

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import DisplayData from '../components/userData';
 import AddData from '../components/insertUser';
-import UpdateData from '../components/updateuser';
+import UpdateData from '../components/updateUser';
 import DeleteData from '../components/deleteUser';
+import Navbar from '../components/Navbar';
 
 const User = () => {
   return (
-    <div className='bg-gray-300 min-h-screen'>
+    <div className='py-12 bg-gray-200 min-h-screen'>
+      <Navbar />
       <DisplayData />
       <div className="flex items-center justify-center mt-10 gap-4">
         <DeleteData />
