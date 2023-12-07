0615601f7e57ed53fb2420204890dd7055f67178
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-12-07 20:02:18 +0700

    feat: added photo page route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import User from "./pages/User";
 import NotFoundPage from "./pages/NotFound";
 import Product from "./pages/Product";
+import Photo from "./pages/Photo";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="*" element={<NotFoundPage />} />
         <Route path="/user" element={<User />} />
         <Route path="/product" element={<Product />} />
+        <Route path="/photo" element={<Photo />} />
       </Routes>
     </Router>
   );
