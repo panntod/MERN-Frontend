c5ce9f6618bd0d4dded4c1f159ebf9b28897faf2
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-12-15 05:44:18 +0700

    feat: added function add to clipboard

--- src/components/photoData.jsx
+++ src/components/photoData.jsx
@@ -15,6 +15,17 @@ const PhotoGallery = () => {
       });
   }, []);
 
+  const copyToClipboard = (text) => {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        alert("Text copied to clipboard");
+      })
+      .catch((err) => {
+        alert("Error copying text to clipboard");
+      });
+  };
+
   return (
     <div className="grid grid-cols-3 gap-4 py-10 mx-6">
       {photos.length === 0 ? (
@@ -25,11 +36,18 @@ const PhotoGallery = () => {
         photos.map((photo) => (
           <div key={photo._id} className="bg-gray-200 p-4 rounded">
             <img
-              src={photo.photoData} 
-              alt={photo._id} 
+              src={photo.photoData}
+              alt={photo._id}
               className="w-full h-auto rounded"
             />
-            <p>Id: {photo._id}</p>
+            <div className="w-full bg-gray-600 p-2 rounded-md shadow-md">
+              <p
+                className="text-center text-white cursor-pointer"
+                onClick={() => copyToClipboard(`${photo._id}`)}
+              >
+                ID: {photo._id}
+              </p>
+            </div>
           </div>
         ))
       )}
